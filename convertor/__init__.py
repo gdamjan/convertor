e102b329ca1b4ccfe95ef3d86216315dcf73253a
Damjan Georgievski <gdamjan@gmail.com>
2009-08-05 22:45:32 +0200

    Major overhaul
    I use a completelly different algorithm now: first step is to enumerate all
    styles in styles.xml and content.xml, noting which ones need to be converted or
    not, which ones inherit the style from a parent-style, and which ones don't
    have a font so need to inherit the style from the parent in the content.
    
    Then a recursive function iterates through the content and converts the element
    text and it's children tails, remembering the parent-content converter function
    as it goes more deeply.

--- convertor/__init__.py
+++ convertor/__init__.py
@@ -1,69 +1,111 @@
-from zipfile import ZipFile
-from collections import namedtuple
-
 from lxml import etree
 
-from util import convert_text, the_stupid_fonts
+from util import the_stupid_fonts, build_namespace_tuple
 from odffile import ODFFile
 
-
-def build_namespace_tuple(nsmap):
-    factory = namedtuple('namespaces', ' '.join(nsmap.keys()))
-    return factory(*['{%s}' % x for x in nsmap.values()])
+# Global sentinels
+INHERIT_CONVERT = object()
+DONT_CONVERT = object()
+NO_PARENT_STYLE = object()
 
 def convert_doc(doc):
-    the_stupid_styles = {}
+    style_mapping = {}
 
     styles = doc.get_xml("styles")
-    styles = convert_styles(styles, the_stupid_styles)
+    convert_styles(styles, style_mapping)
 
     content = doc.get_xml("content")
-    content = convert_styles(content, the_stupid_styles)
-    content = convert_body(content, the_stupid_styles)
+    convert_styles(content, style_mapping)
+    convert_content(content, style_mapping)
 
     doc.set_xml("styles", styles)
     doc.set_xml("content", content)
-    return doc
+    return
 
-def convert_styles(tree, the_stupid_styles):
-    # TODO: style inheritance??? implicit inherit from "Standard"?
+def convert_styles(tree, style_mapping):
+    '''Iterates through all styles defined in the tree and extracts the
+    convertor function to use for that style. 
+    
+    Also the tree will be changed inplace.
+    '''
     root = tree.getroot()
     ns = build_namespace_tuple(root.nsmap)
 
-    # find the styles with the stupid fonts
-    #styles = "/office:document-content|office:document-styles/office:automatic-styles"
-    prop_path = "//style:text-properties[@style:font-name|@style:font-name-complex]"
-    for text_prop in root.xpath(prop_path, namespaces=root.nsmap):
-        font = text_prop.get(ns.style + 'font-name') or text_prop.get(ns.style + 'font-name-complex')
-        font = font.lower()
-        if font in the_stupid_fonts:
-            style_name = text_prop.getparent().get(ns.style + 'name')
-            the_stupid_styles[style_name] = font
-            text_prop.set(ns.style + "font-name", the_stupid_fonts[font][0])
-    return tree
+    for style in root.xpath('//style:default-style|//style:style', namespaces=root.nsmap):
+        if style.tag == ns.style('default-style'):
+            style_name = style.get(ns.style('family'))
+        else:
+            style_name = style.get(ns.style('name'))
+        parent_style_name = style.get(ns.style('parent-style-name'), NO_PARENT_STYLE)
 
-def convert_body(tree, the_stupid_styles):
+        font = None
+        font_xpath = './/style:text-properties[@style:font-name|@style:font-name-complex]'
+        for text_properties in style.xpath(font_xpath, namespaces=root.nsmap):
+            font = text_properties.get(ns.style('font-name')) \
+                    or text_properties.get(ns.style('font-name-complex'))
+
+        if font is None:
+            if parent_style_name in style_mapping:
+                # if the style has no font by itself but has a parent-style that 
+                # had the stupid font then just remember the style and use the 
+                # conversion from the parent style
+                style_mapping[style_name] = style_mapping[parent_style_name]
+                #msg = 'PARENT(%s)' % parent_style_name
+            else:
+                # no font-name, no parent-style in this style
+                # the element will inherit the conversion from 
+                # it's parent in the content *tree*
+                style_mapping[style_name] = INHERIT_CONVERT
+                #msg = 'INHERITED from content parent'
+
+        elif font in the_stupid_fonts:
+            # if a stupid font is used in this style:
+            #  - change the font in the style
+            #  - and remember the style so that we can convert the text later
+            text_properties.set(ns.style('font-name'), the_stupid_fonts[font].replacement_font)
+            style_mapping[style_name] = the_stupid_fonts[font].convert_func
+            #msg = 'REPLACE'
+        else:
+            # there's a font but not a stupid one, use null conversion
+            style_mapping[style_name] = DONT_CONVERT
+            #msg = "DON'T CONVERT"
+        #print msg, '=>', style_name
+    return
+
+def convert_element(el, func):
+    if el.text:
+        el.text = func(el.text)
+    for child in el: 
+        if child.tail:
+            child.tail = func(child.tail)
+    return
+
+def convert_tree(tree, inherited_convert, style_mapping, ns):
+    for el in tree:
+        style = el.get(ns.text('style-name'))
+        convert_func = DONT_CONVERT
+        #if style == "T1":
+        #    raise BaseException, locals()
+        if style in style_mapping:
+            convert_func = style_mapping[style]
+        if convert_func is INHERIT_CONVERT:
+            convert_func = inherited_convert
+        if convert_func is not DONT_CONVERT:
+            convert_element(el, convert_func)
+        convert_tree(el, convert_func, style_mapping, ns)
+
+def convert_content(tree, style_mapping):
     root = tree.getroot()
+    ns = build_namespace_tuple(root.nsmap)
 
-    # find the text with the selected styles, for each style loop the whole tree
-    # maybe it's smarter to iterate the tree and then compare for style
-    # test with really big documents ???
+    text_path = '/office:document-content/office:body/office:text'
+    body = root.xpath(text_path, namespaces=root.nsmap)
 
-    text_path = "/office:document-content/office:body/office:text"
-    text_find = etree.XPath(text_path + '//*[@text:style-name=$name]', namespaces=root.nsmap)
-    #text = root.xpath(text_path, namespaces=root.nsmap)[0]
-    for style, font in the_stupid_styles.items():
-        for para in text_find(root, name=style):
-            if para.text:
-                para.text = convert_text(para.text, font)
-            for child in para:
-                if child.tail:
-                    child.tail = convert_text(child.tail, font)
-    return tree
+    convert_tree(body, DONT_CONVERT, style_mapping, ns)
+    return
 
 
-
-# pretty_print = lambda el: lxml.etree.tostring(el, pretty_print=True)
+# pretty_print = lambda el: etree.tostring(el, pretty_print=True)
 # find = lxml.etree.XPath("//b")
 # tree = lxml.etree.parse(StringIO.StringIO(xml))
 # root = tree.getroot()
